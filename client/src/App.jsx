77a6a9fb694c17e59a238be5ca0bd6355c435a13
Manju760-arch <manjusivaa6634@gmail.com>
2025-12-12 20:09:20 +0530

    Inital commit

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -9,7 +9,7 @@ import { AuthContext } from '../context/AuthContext'
 const App = () => {
   const {authUser} =useContext(AuthContext)
   return (
-    <div className="bg-[url('./src/assets/bg_img1.jpg')] bg-contain">
+    <div className="bg-[url('/bg_img1.jpg')] bg-contain">
       <Toaster/>
       <Routes>
      <Route path='/' element={authUser ? <HomePage /> : <Navigate to="/login" />}/>
--- server/server.js
+++ server/server.js
@@ -54,7 +54,11 @@ try {
 } catch (err) {
   console.error("MongoDB connection error:", err.message);
 }
-
-// Start server
+if(process.env.NODE_ENV !=="production"){
+  // Start server
 const PORT = process.env.PORT || 5000;
 server.listen(PORT, () => console.log(`Server running on PORT: ${PORT}`));
+}
+//export server for vercel
+export default server;
+
